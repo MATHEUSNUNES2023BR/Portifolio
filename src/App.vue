9df1062d0047cd083eccb374f32fcd5e517f8e74
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-02-15 18:06:08 -0300

    Criando o Main

--- src/App.vue
+++ src/App.vue
@@ -32,17 +32,36 @@
     </div>
   </nav>
 
-  <main>
-     
+  <main id="Main" class="container-fluid">
+    <div class="row mt-4">
+      <div class="col-sm-5 col-12 d-flex justify-content-center">
+        <img class="Minha-Foto rounded-circle" src="./assets/midia/minha-foto.jpg" alt="">
+      </div>
+      <div class="Frase-e-icones col mt-sm-5">
+        <h2>Olá sou Matheus,</h2>
+        <h2>Desenvolvedor Web</h2>
+        <ul class="justify-content-between d-flex mt-sm-5">
+          <li> <img src="./assets/midia/icons/html-5-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/css-3-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/js-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/node-js-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/bootstrap-4-logo-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/vue-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/sass-svgrepo-com.svg" alt=""> </li>
+          <li> <img src="./assets/midia/icons/grunt-svgrepo-com.svg" alt=""> </li>
+        </ul>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
+  /* Configurações globais e para Desktops ou TVs*/
   #Nav{
     background-color: var(--Nav-Background);
     box-shadow: 0 1px 6px 4px var(--Box-Shadow-Color);
   }
-  .nav-link{
+  #Nav .nav-link{
     font-family: "Roboto", sans-serif;
     text-transform: uppercase;
     font-size: var(--Nav-Font);
@@ -52,4 +71,20 @@
       transition: 0.3s ease; 
     }
   }
+  .Frase-e-icones ul li img{
+      animation: rotateHorizontal 5s infinite linear;
+  }
+
+  .Frase-e-icones ul{
+      list-style: none;
+  }
+
+  @keyframes rotateHorizontal {
+    0% {
+      transform: rotateY(0deg);
+    }
+    100% {
+      transform: rotateY(360deg);
+    }
+  } 
 </style>
